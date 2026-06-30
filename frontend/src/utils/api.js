0e4aaa2f7d20df7e196daf34488acf1117ca7fd4
akashmirase <akashmirase6@gmail.com>
2026-06-30 21:05:03 +0530

    Connect frontend to production backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://life-pilot-1.onrender.com/api',
   withCredentials: true,
   timeout: 30000,
 });
